8b03956c56a4a1ef9854edb4d9c4a43f2856e89d
Mirzzie <mirzad.ismail@gmail.com>
2022-12-26 10:13:57 +0530

    fix3: button

--- admin/add_schedview.php
+++ admin/add_schedview.php
@@ -242,7 +242,7 @@ if(isset($_REQUEST['view'])){
           echo "<td>".$row['time']."</td>";
           echo "<td>".$row['dname']."</td>";
           echo "<td>".$row['sub_name']."</td>";
-          echo "<td><button class='btn-btn-primary'><a href='allocate.php?table_id=$table_id'</a>Allocate</button></td>";
+          echo "<td><button type='button' class='btn-btn-primary'><a href='allocate.php?table_id=$table_id'></a>Allocate</button></td>";
           echo "</tr></tbody>";
         }
  echo "</table></div>";
