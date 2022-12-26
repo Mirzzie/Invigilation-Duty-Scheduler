77ad6892981264db32860988c0f01b3c86aa039b
Mirzzie <mirzad.ismail@gmail.com>
2022-12-26 08:55:09 +0530

    check1

--- admin/add_schedview.php
+++ admin/add_schedview.php
@@ -251,6 +251,7 @@ if(isset($_REQUEST['view'])){
  echo "</table></div>";
   }
 ?>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
 
